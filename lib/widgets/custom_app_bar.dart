b49c1db4faa245cc68ba16c2c0b2cfc4381d8d51
mtarek20 <m.tarek2040@gmil.com>
2023-05-12 08:34:16 +0300

    create notes app

---
+++ lib/widgets/custom_app_bar.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/widgets/custom_search_icon.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: const [
+        Text(
+          'Notes',
+          style: TextStyle(color: Colors.white, fontSize: 30),
+        ),
+        Spacer(),
+        CustomSearchIcon(),
+      ],
+    );
+  }
+}
